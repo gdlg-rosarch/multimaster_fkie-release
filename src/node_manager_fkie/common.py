20f366e68855ba899b6b040e00ba187a86eaf726
Alexander Tiderko <Alexander.Tiderko@googlemail.com>
2013-09-06 11:26:39 +0200

    Rebase from 'release/groovy/node_manager_fkie'

--- src/node_manager_fkie/common.py
+++ src/node_manager_fkie/common.py
@@ -3,6 +3,13 @@ import os
 MANIFEST_FILE = 'manifest.xml'
 PACKAGE_FILE = 'package.xml'
 
+try:
+  from catkin_pkg.package import parse_package
+  CATKIN_SUPPORTED = True
+except ImportError:
+  CATKIN_SUPPORTED = False
+
+PACKAGE_CACHE = {}
 
 def get_ros_home():
   '''
@@ -48,26 +55,45 @@ def get_packages(path):
   result = {}
   if os.path.isdir(path):
     fileList = os.listdir(path)
-    if MANIFEST_FILE in fileList or PACKAGE_FILE in fileList:
+    if MANIFEST_FILE in fileList:
       return {os.path.basename(path) : path}
+    if CATKIN_SUPPORTED and PACKAGE_FILE in fileList:
+      try:
+        pkg = parse_package(path)
+        return {pkg.name : path}
+      except:
+        pass
+      return {}
     for f in fileList:
-      ret = get_packages(os.path.sep.join([path, f]))
+      ret = get_packages(os.path.join(path, f))
       result = dict(ret.items() + result.items())
   return result
 
 def package_name(dir):
   '''
   Returns for given directory a tuple of package name and package path or None values.
+  The results are cached!
   @rtype: C{(name, path)}
   '''
   if not (dir is None) and dir and dir != os.path.sep and os.path.isdir(dir):
+    if PACKAGE_CACHE.has_key(dir):
+      return PACKAGE_CACHE[dir]
     package = os.path.basename(dir)
     fileList = os.listdir(dir)
     for file in fileList:
-      if file == MANIFEST_FILE or file == PACKAGE_FILE:
+      if file == MANIFEST_FILE:
+        PACKAGE_CACHE[dir] = (package, dir)
         return (package, dir)
-    return package_name(os.path.dirname(dir))
+      if CATKIN_SUPPORTED and file == PACKAGE_FILE:
+        try:
+          pkg = parse_package(os.path.join(dir, file))
+          PACKAGE_CACHE[dir] = (pkg.name, dir)
+          return (pkg.name, dir)
+        except:
+          return (None,None)
+    PACKAGE_CACHE[dir] = package_name(os.path.dirname(dir))
+    return PACKAGE_CACHE[dir]
   return (None, None)
 
 def is_package(file_list):
-  return (MANIFEST_FILE in file_list or PACKAGE_FILE in file_list)
+  return (MANIFEST_FILE in file_list or (CATKIN_SUPPORTED and PACKAGE_FILE in file_list))
